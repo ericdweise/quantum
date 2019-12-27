96c9969fbc3b93f267017ebd4a929b723f1fb59e
Eric Weise <ericdweise@gmail.com>
2019-12-27 12:44:19 -0800

    saved my own methods for visualizing matrices and state vectors

---
+++ qiskit/myqiskit/test.py
@@ -0,0 +1,52 @@
+import unittest
+from math import pi, e, sqrt
+
+from numpy import complex
+
+from visualize import complex_pretty
+
+class ComplexPrettyStrig(unittest.TestCase):
+
+    def test_zero(self):
+        n = complex(0,0)
+        self.assertEqual(complex_pretty(n), '0')
+
+    def test_real_int(self):
+        n = complex(4,0)
+        self.assertEqual(complex_pretty(n), '4')
+
+    def test_real_dec(self):
+        n = complex(pi,0)
+        self.assertEqual(complex_pretty(n), '3.14')
+
+    def test_real_neg(self):
+        n = complex(-3,0)
+        self.assertEqual(complex_pretty(n), '-3')
+
+    def test_imag_int(self):
+        n = complex(0,4)
+        self.assertEqual(complex_pretty(n), '4j')
+
+    def test_imag_dec(self):
+        n = complex(0,e)
+        self.assertEqual(complex_pretty(n), '2.72j')
+
+    def test_imag_neg(self):
+        n = complex(0,-6)
+        self.assertEqual(complex_pretty(n), '-6j')
+
+    def test_complex_int(self):
+        n = complex(4,3)
+        self.assertEqual(complex_pretty(n), '4 + 3j')
+
+    def test_complex_dec(self):
+        n = complex(1/sqrt(2), 1/sqrt(2))
+        self.assertEqual(complex_pretty(n), '0.71 + 0.71j')
+
+    def test_complex_neg(self):
+        n = complex(-0.5, -sqrt(3)/2)
+        self.assertEqual(complex_pretty(n), '-0.50 - 0.87j')
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ qiskit/myqiskit/visualize.py
@@ -0,0 +1,67 @@
+from math import floor
+
+from qiskit import *
+from IPython.display import display, Markdown, Latex
+
+
+unitary_simulator = Aer.get_backend('unitary_simulator')
+statevector_simulator = Aer.get_backend('statevector_simulator')
+
+
+def complex_pretty(c):
+    if c.imag == 0 and c.real == 0:
+        return '0'
+
+    elif c.imag == 0:
+        if c.real == floor(c.real):
+            return str(int(c.real))
+        else:
+            return f'{c.real:.2f}'
+
+    elif c.real == 0:
+        if c.imag == floor(c.imag):
+            return f'{int(c.imag)}j'
+        else:
+            return f'{c.imag:.2f}j'
+
+    if c.real == floor(c.real):
+        rp = f'{int(c.real)}'
+    else:
+        rp = f'{c.real:.2f}'
+
+    if c.imag > 0:
+        op = ' + '
+        if c.imag == floor(c.imag):
+            ip = f'{int(c.imag)}'
+        else:
+            ip = f'{c.imag:.2f}'
+
+    else:
+        op = ' - '
+        if c.imag == floor(c.imag):
+            ip = f'{-int(c.imag)}'
+        else:
+            ip = f'{-c.imag:.2f}'
+
+    return(rp + op + ip + 'j')
+
+
+def matrix_pretty(circuit):
+    job = execute(circuit, unitary_simulator)
+    result = job.result()
+    gate = result.get_unitary()
+    gate_latex = '\\begin{pmatrix}'
+    for line in gate:
+        for element in line:
+            gate_latex += complex_pretty(element) + '&'
+        gate_latex  = gate_latex[0:-1]
+        gate_latex +=  '\\\\'
+    gate_latex  = gate_latex[0:-2]
+    gate_latex += '\end{pmatrix}'
+    display(Markdown(gate_latex))
+
+
+def statevector(circuit):
+    job = execute(circuit, statevector_simulator)
+    result = job.result()
+    return result.get_statevector()
